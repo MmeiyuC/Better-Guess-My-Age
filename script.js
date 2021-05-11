7b5dca9f59583bf572adde48f99a3d32737da954
MmeiyuC <>
2021-05-11 17:56:48 +0000

    edited

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let age = 16
 let guess = 0
 
 // Connect "Start" button to function
 document.getElementById('start').addEventListener('click', guessmyage)
 
 function guessmyage () {
+  let age = 16
   // Set while loop
   while (guess !== age) {
     guess = prompt('Guess a number between 10 and 20')
