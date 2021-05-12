ea1f086bf6cf1b1434efb2932fa427eff38d6345
MmeiyuC <>
2021-05-12 14:08:09 +0000

    edited

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-let age = 16
+const age = 16
 let guess = 0
 
 // Connect "Start" button to function
 document.getElementById('start').addEventListener('click', guessmyage)
 
 function guessmyage () {
-
   // Set while loop
-    while (guess != age) {
-      guess = parseInt(prompt('Guess a number between 10 and 20'))
-      if (guess === age) {
-        alert('You got it right!')
-      } else if (guess < age) {
-        alert('Too young')
-      } else if (guess > age) {
-        alert('Too old')
-      }
+  while (guess !== age) {
+    guess = parseInt(prompt('Guess a number between 10 and 20'))
+    if (guess === age) {
+      alert('You got it right!')
+    } else if (guess < age) {
+      alert('Too young')
+    } else if (guess > age) {
+      alert('Too old')
     }
+  }
 }
